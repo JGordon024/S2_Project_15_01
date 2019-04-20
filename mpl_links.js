85dff0c8a0e5e8f2da163284a4940d71f0db5b81
Unknown <jgordon024@west-mec.org>
2019-04-19 17:16:02 -0700

    Final commit
    Finished function, I added the onchange event to the index of allSelect and set that to equal the event target value.

--- mpl_links.js
+++ mpl_links.js
@@ -11,12 +11,12 @@
    Filename: mpl_links.js
 
 */
-
+//Adds an event listener to the window that runs a function when the page loads, The funciton creates a variable that is linked to the form govLinks. A for loop is run for the variable and when the variable is changed, another anonymous function is ran that targets the value of the event
 window.addEventListener("load", function () {
     var allSelect = document.forms.govLinks
     for (var i = 0; i < allSelect.length; i++) {
-        document.querySelector("").addEventListener("onchange", function () {
-
-        })
+        allSelect[i].onchange = function (e) {
+            document.location.href = e.target.value;
+        }
     }
 })
